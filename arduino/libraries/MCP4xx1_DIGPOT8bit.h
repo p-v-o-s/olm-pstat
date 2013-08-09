971b98928654df94d5d26800d3fb91fc02b419c8
Craig Wm. Versek <cversek+github@gmail.com>
2013-08-09 13:15:01 -0400

    Dig Pot library functionality

---
+++ arduino/libraries/MCP4xx1_DIGPOT8bit.h
@@ -0,0 +1,33 @@
+/*
+  MCP4xx1_DIGPOT8bit_test.ino - Library for interacting with the Microchip
+                       MCP4[12]x1 8-Bit Digital Single (41x1) and Dual (42x1)
+                       Potentiometers with SPI Interface.
+  Created by Craig Wm. Versek, 2013-07-04
+  Released into the public domain.
+*/
+
+#ifndef _MCP4xx1_DIGPOT8bit_H_INCLUDED
+#define _MCP4xx1_DIGPOT8bit_H_INCLUDED
+
+#include <Arduino.h>
+
+#define RES8BIT 256
+
+class MCP4xx1_DIGPOT8bitClass {
+public:
+  MCP4xx1_DIGPOT8bitClass(int   slaveSelectLowPin,
+                          float maxResistance
+                         );
+  //Configuration methods
+  void begin(); // Default
+  void end();
+  //Functionality methods
+  int writeVolatileWiper0(unsigned int N);
+  int writeVolatileWiper1(unsigned int N);
+private:
+  int   _slaveSelectLowPin;
+  float _maxResistance;
+};
+
+
+#endif //_MCP4xx1_DIGPOT8bit_H_INCLUDED
