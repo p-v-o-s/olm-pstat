e8a6f9f16eb6cba494974e76994e7b3360176c05
Craig Versek <cversek@macbuntu.(none)>
2013-11-24 02:03:07 -0500

    prepping for exportation of sweep data

--- python/src/OLMpstat/apps/control/application.py
+++ python/src/OLMpstat/apps/control/application.py
@@ -46,7 +46,15 @@ class Application(ApplicationBase):
                                         current_range_level = current_range_level,
                                         )
         #creat new or overwrite the last dataset
-        self._vsweep_dataset = VoltageSweepDataSet()
+        md = OrderedDict()
+        md['start_timestamp'] = time.time()
+        md['v_start']         = v_start
+        md['v_end']           = v_end
+        md['v_rate']          = v_rate
+        md['samp_rate']       = samp_rate
+        md['cycles']          = cycles
+        md['current_range_level'] = current_range_level
+        self._vsweep_dataset = VoltageSweepDataSet(metadata = md)
         voltage_sweep.start() #this should start a non-blocking thread
         
     def _append_vsweep_data_record(self,
@@ -59,49 +67,13 @@ class Application(ApplicationBase):
                                             WEtoRE_voltage,
                                             WE_current)
                                           )
-
-#import numpy as np
-#import pylab
-#import serial, yaml
-#from StringIO import StringIO
-#from potentiostat import Potentiostat
-
-#PORT     = "/dev/ttyUSB0"
-#BAUDRATE = 115200
-
-#if __name__ == "__main__":
-#    pstat = Potentiostat(port     = PORT,
-#                         baudrate = BAUDRATE,
-#                         debug    = True)
-#    R1 = [1.0]
-#    fig1 = pylab.figure()
-#    ax1_1 = fig1.add_subplot(211)
-#    ax1_2 = fig1.add_subplot(212)
-#    fig1.suptitle("Potentiostatic Control of R2 = 1kOhm Load,\n with varying R1 (CE to WE)")
-#    ax1_1.set_xlabel("Voltage WE (vs. RE) [V]")
-#    ax1_1.set_ylabel("Voltage CTRL [V]")
-#    ax1_1.set_xlim(-1.6,1.6)
-#    ax1_2.set_xlabel("Voltage WE (vs. RE) [V]")
-#    ax1_2.set_ylabel("Current WE [mA]")
-#    ax1_2.set_xlim(-1.6,1.6)
-#    for r1 in R1:
-#        print "Change resistor R1 to %0.2f kOhm" % r1
-#        raw_input()
-#        pstat.reset()
-#        D = pstat.do_voltage_sweep(v_start   = 0.0,
-#                                   v_end     = 1.0,
-#                                   v_rate    = 0.25,
-#                                   samp_rate = 10.0,
-#                                   cycles    = 3,
-#                                  )
-#        V_CTRL   = D[:,0]
-#        V_WEtoRE = D[:,1]
-#        I_WE     = D[:,2] * 1000.0 #convert to mAmps
-#        #plot voltage RE vs. input V_CE
-#        ax1_1.plot(V_WEtoRE,V_CTRL,'.-', label = "%0.2f kOhm" % r1)
-#        #plot voltage RE vs. input V_CE
-#        ax1_2.plot(V_WEtoRE,I_WE,'.-', label = "%0.2f kOhm" % r1)
-#    ax1_1.legend(loc="lower right", prop={'size':8})
-#    pylab.savefig("data/out.pdf")
-#    pylab.savefig("data/out.png")
-#    pylab.show()
+                                          
+    def export_data(self, filename):
+        if   filename.endswith(".csv"):
+            self._vsweep_dataset.to_csv_file(filename)
+        elif filename.endswith(".db"):
+            self._vsweep_dataset.to_shelf(filename)
+        elif filename.endswith(".hd5"):
+            raise NotImplementedError("HDF5 formatting is not ready, please check back later!")
+        else:
+            raise ValueError("the filename extension was not recognized, it must end with: .csv, .db, or .hd5")
